df746fc76e9defca8f1505b354559340dbbdfba2
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-05-10 08:42:26 +0530

    Update roboshop-ansible/

--- create-ec2.sh
+++ create-ec2.sh
@@ -16,14 +16,13 @@ aws ec2 describe-spot-instance-requests --filters Name=tag:Name,Values=${NAME} N
 if [ $? -eq 0 ]; then
   echo "Instance Already exists"
   exit 0
+else
+  AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" --output table  | grep ImageId | awk '{print $4}')
+  aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${NAME}}]" "ResourceType=instance,Tags=[{Key=Name,Value=${NAME}}]" --security-group-ids sg-052da698a233eaebe &>/dev/null
+  echo EC2 Instance Created
+  sleep 30
 fi
 
-AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" --output table  | grep ImageId | awk '{print $4}')
-
-aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${NAME}}]" "ResourceType=instance,Tags=[{Key=Name,Value=${NAME}}]" --security-group-ids sg-052da698a233eaebe &>/dev/null
-echo EC2 Instance Created
-
-sleep 30
 INSTANCE_ID=$(aws ec2 describe-spot-instance-requests --filters Name=tag:Name,Values=${NAME} Name=state,Values=active --output table | grep InstanceId | awk '{print $4}')
 
 IPADDRESS=$(aws ec2 describe-instances  --instance-ids ${INSTANCE_ID}  --output table | grep PrivateIpAddress | head -n 1 | awk '{print $4}')
