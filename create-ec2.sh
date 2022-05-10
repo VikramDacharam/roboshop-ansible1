c066f1213e6953b01e7020d508c60858d95ec0b8
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-05-10 08:42:45 +0530

    Update roboshop-ansible/

--- create-ec2.sh
+++ create-ec2.sh
@@ -15,7 +15,6 @@ NAME=$1
 aws ec2 describe-spot-instance-requests --filters Name=tag:Name,Values=${NAME} Name=state,Values=active --output table | grep InstanceId &>/dev/null
 if [ $? -eq 0 ]; then
   echo "Instance Already exists"
-  exit 0
 else
   AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" --output table  | grep ImageId | awk '{print $4}')
   aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${NAME}}]" "ResourceType=instance,Tags=[{Key=Name,Value=${NAME}}]" --security-group-ids sg-052da698a233eaebe &>/dev/null
